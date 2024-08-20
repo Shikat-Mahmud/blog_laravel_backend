60f6dc229ea1bff8af79f3e850d239592fe734fc
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2024-08-20 23:09:22 +0600

    controller updated to handle image upload

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -8,32 +8,56 @@ use Illuminate\Support\Facades\Validator;
 
 class BlogController extends Controller
 {
-    public function store(Request $request){
-        $validator = Validator::make($request->all(), [
-            'title' => 'required|min:10',
-            'author' => 'required|min:3'
-        ]);
+    public function store(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'title' => 'required|min:10',
+                'image' => 'nullable|image',
+                'author' => 'required|min:3'
+            ]);
 
-        if($validator->fails()){
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => 'false',
+                    'message' => 'fix the errors.',
+                    'errors' => $validator->errors()
+                ], 422);
+            }
+
+            $blog = new Blog;
+
+            $blog->title = $request->title;
+            $blog->author = $request->author;
+            $blog->shortDesc = $request->shortDesc;
+            $blog->description = $request->description;
+
+            if ($request->hasFile('image')) {
+                $image = $request->image;
+                $extension = $image->getClientOriginalExtension();
+                $imageName = 'blog' . time() . '.' . $extension;
+                $blog->image = $imageName;
+    
+                // Save image
+                $image->move(public_path('img'), $imageName);
+            }
+
+            $blog->save();
+
+            return response()->json([
+                'status' => 'true',
+                'message' => 'Blog created successfully.',
+                'data' => $blog
+            ]);
+        } catch (\Exception $e) {
+            // Log the exception message
+            \Log::error("Error in store method: " . $e->getMessage());
+
+            // Return a JSON response with the error
             return response()->json([
                 'status' => 'false',
-                'message' => 'fix the errors.',
-                'errors' => $validator->errors()
-            ]);
+                'message' => 'An unexpected error occurred.',
+            ], 500);
         }
-
-        $blog = new Blog;
-
-        $blog->title = $request->title;
-        $blog->author = $request->author;
-        $blog->shortDesc = $request->shortDesc;
-        $blog->description = $request->description;
-        $blog->save();
-
-        return response()->json([
-            'status' => 'true',
-            'message' => 'Blog created successfully.',
-            'data' => $blog
-        ]);
     }
 }
